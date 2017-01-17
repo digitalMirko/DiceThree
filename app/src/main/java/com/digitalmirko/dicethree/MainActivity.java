45d1644dd559310f18add32a85fb74a1e0b52cb1
Mirko Cukich <mirkocukich@gmail.com>
2017-01-17 16:19:27 -0600

    Material Design button

--- app/src/main/java/com/digitalmirko/dicethree/MainActivity.java
+++ app/src/main/java/com/digitalmirko/dicethree/MainActivity.java
@@ -3,13 +3,11 @@ package com.digitalmirko.dicethree;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     TextView rollResult;
 
     // Field to hold the roll button
-    Button rollButton;
+//    Button rollButton;
 
     // Field to hold the score
     int score;
@@ -56,8 +54,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                rollDice(view);
             }
         });
 
@@ -67,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         rollResult = (TextView) findViewById(R.id.rollResult);
-        rollButton = (Button) findViewById(R.id.rollButton);
+//        rollButton = (Button) findViewById(R.id.rollButton);
         scoreText = (TextView) findViewById(R.id.scoreText);
 
         // Initialize the random number generator
